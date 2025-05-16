173994f1e340a337cbf96cd9f6cfb32b829a89cd
NB0524-4195\FSS <komkit.n@fnsyrus.com>
2025-05-16 16:40:30 +0700

    #5 First Step

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-## PROJEcT
+## MY IMPROVE SKILL PROJEcT 1
 
 # Node version 22.17.0
 # npm install
 # npm start
+
+# Swipe daily task for recheck myself
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/main.css';
+import './css/button.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+        Halo
+        <div className='div-button-swipe'>
+          <button className="btn btn-left">DONE</button>
+          <button className="btn btn-right">LAZY</button>
+        </div>
     </div>
   );
 }
